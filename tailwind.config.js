4d594122055f51f69c5a72b040d67c91bfe4f4e7
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-01-01 13:50:38 +0700

    2.1 List Pesanan Page Slicing & 2.2 Home Page Slicing Menu
    Ini merupakan commit saya untuk menyelesaikan pengerjaan slicing ui halaman list pesanan dan slicing halaman home.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,26 @@ module.exports = {
         inputcoolfresh : '#F0F4FD',
         btncoolfresh : '#041172',
         textcoolfresh2 : '#535A73',
+        abutulisancoolfresh : '#272323',
+        langitbirucoolfresh: '#27AAE1',
+        backgroundabu: '#F9F9F9',
       },
       backgroundImage: {
         'loginpattern': "url('/src/img/bg_login.png')",
+        'menupattern': "url('/src/img/menubg_halamancoolfresh.png')",
       }
+    },
+    fontSize: {
+      'xxs': '0.5rem',
+      'xs' : '0.75rem',
+      sm: '0.8rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
     screens: {
       'smalldevices': '640px',
@@ -19,7 +35,10 @@ module.exports = {
     },
     fontfamily: {
       'poppins': ['Poppins', 'sans-serif']
-    }
+    },
+    flex: {
+      '2': '2 2 0%'
+    },
   },
   plugins: [],
 }
